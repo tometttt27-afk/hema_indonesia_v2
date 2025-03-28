f26f3be052163ab5b7668be0351339f558c33b23
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-03-28 16:25:15 +0700

    Add template admin and add page:dashboard

---
+++ resources/views/template/layout-admin.blade.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="description" content="POS - Bootstrap Admin Template">
+    <meta name="keywords"
+        content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern,  html5, responsive">
+    <meta name="author" content="Dreamguys - Bootstrap Admin Template">
+    <meta name="robots" content="noindex, nofollow">
+    <title>@yield('title_web')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="{{ asset('admin/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/font/urbanist.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
+</head>
+
+<body>
+    <div id="global-loader">
+        <div class="whirly-loader"> </div>
+    </div>
+
+    <div class="main-wrapper">
+
+        <div class="header">
+
+            <div class="header-left active">
+                <a href="index.html" class="logo">
+                    <img src="admin/img/logo.png" alt="">
+                </a>
+                <a href="index.html" class="logo-small">
+                    <img src="admin/img/logo-small.png" alt="">
+                </a>
+                <a id="toggle_btn" href="javascript:void(0);">
+                </a>
+            </div>
+
+            <a id="mobile_btn" class="mobile_btn" href="#sidebar">
+                <span class="bar-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+            </a>
+
+            <ul class="nav user-menu">
+
+                <li class="nav-item">
+                    <div class="top-nav-search">
+                        <a href="javascript:void(0);" class="responsive-search">
+                            <i class="fa fa-search"></i>
+                        </a>
+                        <form action="#">
+                            <div class="searchinputs">
+                                <input type="text" placeholder="Search Here ...">
+                                <div class="search-addon">
+                                    <span><img src="admin/img/icons/closes.svg" alt="img"></span>
+                                </div>
+                            </div>
+                            <a class="btn" id="searchdiv"><img src="admin/img/icons/search.svg" alt="img"></a>
+                        </form>
+                    </div>
+                </li>
+
+
+                <li class="nav-item dropdown has-arrow flag-nav">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="javascript:void(0);"
+                        role="button">
+                        <img src="admin/img/flags/us1.png" alt="" height="20">
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a href="javascript:void(0);" class="dropdown-item">
+                            <img src="admin/img/flags/us.png" alt="" height="16"> English
+                        </a>
+                    </div>
+                </li>
+
+
+                <li class="nav-item dropdown">
+                    <a href="javascript:void(0);" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+                        <img src="admin/img/icons/notification-bing.svg" alt="img"> <span
+                            class="badge rounded-pill">4</span>
+                    </a>
+                    <div class="dropdown-menu notifications">
+                        <div class="topnav-dropdown-header">
+                            <span class="notification-title">Notifications</span>
+                            <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+                        </div>
+                        <div class="noti-content">
+                            <ul class="notification-list">
+                                <li class="notification-message">
+                                    <a href="activities.html">
+                                        <div class="media d-flex">
+                                            <span class="avatar flex-shrink-0">
+                                                <img alt="" src="admin/img/profiles/avatar-02.jpg">
+                                            </span>
+                                            <div class="media-body flex-grow-1">
+                                                <p class="noti-details"><span class="noti-title">John Doe</span> added
+                                                    new task <span class="noti-title">Patient appointment
+                                                        booking</span></p>
+                                                <p class="noti-time"><span class="notification-time">4 mins ago</span>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="notification-message">
+                                    <a href="activities.html">
+                                        <div class="media d-flex">
+                                            <span class="avatar flex-shrink-0">
+                                                <img alt="" src="admin/img/profiles/avatar-03.jpg">
+                                            </span>
+                                            <div class="media-body flex-grow-1">
+                                                <p class="noti-details"><span class="noti-title">Tarah
+                                                        Shropshire</span> changed the task name <span
+                                                        class="noti-title">Appointment booking with payment
+                                                        gateway</span></p>
+                                                <p class="noti-time"><span class="notification-time">6 mins ago</span>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="notification-message">
+                                    <a href="activities.html">
+                                        <div class="media d-flex">
+                                            <span class="avatar flex-shrink-0">
+                                                <img alt="" src="admin/img/profiles/avatar-06.jpg">
+                                            </span>
+                                            <div class="media-body flex-grow-1">
+                                                <p class="noti-details"><span class="noti-title">Misty Tison</span>
+                                                    added <span class="noti-title">Domenic Houston</span> and <span
+                                                        class="noti-title">Claire Mapes</span> to project <span
+                                                        class="noti-title">Doctor available module</span></p>
+                                                <p class="noti-time"><span class="notification-time">8 mins ago</span>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="notification-message">
+                                    <a href="activities.html">
+                                        <div class="media d-flex">
+                                            <span class="avatar flex-shrink-0">
+                                                <img alt="" src="admin/img/profiles/avatar-17.jpg">
+                                            </span>
+                                            <div class="media-body flex-grow-1">
+                                                <p class="noti-details"><span class="noti-title">Rolland Webber</span>
+                                                    completed task <span class="noti-title">Patient and Doctor video
+                                                        conferencing</span></p>
+                                                <p class="noti-time"><span class="notification-time">12 mins
+                                                        ago</span></p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="notification-message">
+                                    <a href="activities.html">
+                                        <div class="media d-flex">
+                                            <span class="avatar flex-shrink-0">
+                                                <img alt="" src="admin/img/profiles/avatar-13.jpg">
+                                            </span>
+                                            <div class="media-body flex-grow-1">
+                                                <p class="noti-details"><span class="noti-title">Bernardo
+                                                        Galaviz</span> added new task <span class="noti-title">Private
+                                                        chat module</span></p>
+                                                <p class="noti-time"><span class="notification-time">2 days ago</span>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="topnav-dropdown-footer">
+                            <a href="activities.html">View all Notifications</a>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown has-arrow main-drop">
+                    <a href="javascript:void(0);" class="dropdown-toggle nav-link userset" data-bs-toggle="dropdown">
+                        <span class="user-img"><img src="admin/img/profiles/avator1.jpg" alt="">
+                            <span class="status online"></span></span>
+                    </a>
+                    <div class="dropdown-menu menu-drop-user">
+                        <div class="profilename">
+                            <div class="profileset">
+                                <div class="profilesets">
+                                    <h6>{{ auth()->user()->email }}</h6>
+                                    <h5>{{ auth()->user()->role }}</h5>
+                                </div>
+                            </div>
+                            <hr class="m-0">
+                            <a class="dropdown-item" href="profile.html"> <i class="me-2" data-feather="user"></i>
+                                My Profile</a>
+                            <a class="dropdown-item" href="generalsettings.html"><i class="me-2"
+                                    data-feather="settings"></i>Settings</a>
+                            <hr class="m-0">
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button class="dropdown-item logout pb-0" type="submit"><i
+                                        class="fa-solid fa-right-from-bracket me-2"></i> Logout</button>
+                            </form>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+
+            <div class="dropdown mobile-user-menu">
+                <a href="javascript:void(0);" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
+                    aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a class="dropdown-item" href="profile.html">My Profile</a>
+                    <a class="dropdown-item" href="generalsettings.html">Settings</a>
+                    <a class="dropdown-item" href="signin.html">Logout</a>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="sidebar" id="sidebar">
+            <div class="sidebar-inner slimscroll">
+                <div id="sidebar-menu" class="sidebar-menu">
+                    <ul>
+                        <li class="active">
+                            <a href="{{ url('dashboard') }}"><img src="admin/img/icons/dashboard.svg"
+                                    alt="img"><span>
+                                    Dashboard</span> </a>
+                        </li>
+                        <li class="submenu">
+                            <a href="javascript:void(0);"><img src="admin/img/icons/product.svg"
+                                    alt="img"><span> Product</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="productlist.html">Product List</a></li>
+                                <li><a href="addproduct.html">Add Product</a></li>
+                                <li><a href="categorylist.html">Category List</a></li>
+                                <li><a href="addcategory.html">Add Category</a></li>
+                                <li><a href="subcategorylist.html">Sub Category List</a></li>
+                                <li><a href="subaddcategory.html">Add Sub Category</a></li>
+                                <li><a href="brandlist.html">Brand List</a></li>
+                                <li><a href="addbrand.html">Add Brand</a></li>
+                                <li><a href="importproduct.html">Import Products</a></li>
+                                <li><a href="barcode.html">Print Barcode</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="javascript:void(0);"><img src="admin/img/icons/sales1.svg" alt="img"><span>
+                                    Sales</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="saleslist.html">Sales List</a></li>
+                                <li><a href="pos.html">POS</a></li>
+                                <li><a href="pos.html">New Sales</a></li>
+                                <li><a href="salesreturnlists.html">Sales Return List</a></li>
+                                <li><a href="createsalesreturns.html">New Sales Return</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="javascript:void(0);"><i class="bi bi-hdd-stack"></i>
+                                <span>
+                                    Data Master</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="newuser.html">Data Users</a></li>
+                                <li><a href="userlists.html">Data Gallery</a></li>
+                                <li><a href="userlists.html">Data FAQ</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="javascript:void(0);"><img src="admin/img/icons/time.svg" alt="img"><span>
+                                    Report</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="purchaseorderreport.html">Purchase order report</a></li>
+                                <li><a href="inventoryreport.html">Inventory Report</a></li>
+                                <li><a href="salesreport.html">Sales Report</a></li>
+                                <li><a href="invoicereport.html">Invoice Report</a></li>
+                                <li><a href="purchasereport.html">Purchase Report</a></li>
+                                <li><a href="supplierreport.html">Supplier Report</a></li>
+                                <li><a href="customerreport.html">Customer Report</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="javascript:void(0);"><img src="admin/img/icons/settings.svg"
+                                    alt="img"><span> Settings</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="generalsettings.html">General Settings</a></li>
+                                <li><a href="emailsettings.html">Email Settings</a></li>
+                                <li><a href="paymentsettings.html">Payment Settings</a></li>
+                                <li><a href="currencysettings.html">Currency Settings</a></li>
+                                <li><a href="grouppermissions.html">Group Permissions</a></li>
+                                <li><a href="taxrates.html">Tax Rates</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="page-wrapper">
+            <div class="content">
+                @yield('content-admin')
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('admin/js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('admin/js/feather.min.js') }}"></script>
+    <script src="{{ asset('admin/js/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{ asset('admin/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('admin/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/plugins/apexchart/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('admin/plugins/apexchart/chart-data.js') }}"></script>
+    <script src="{{ asset('admin/js/script.js') }}"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\MainAdminController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,8 @@ Route::get('/gallery', [MainController::class, 'gallery']);
 Route::get('/faq', [MainController::class, 'faq']);
 
 Route::middleware('auth')->group(function () {
-    Route::middleware('role:admin')->group(function () {});
+    Route::middleware('role:admin')->group(function () {
+        Route::get('/dashboard', [MainAdminController::class, 'index']);
+    });
     Route::middleware('role:customer')->group(function () {});
 });
